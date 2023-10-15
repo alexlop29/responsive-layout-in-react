71abe605501180446192b804dfb415e8d32e57ba
alopez-axiom <alopez@axiomcloud.ai>
2023-10-15 16:45:42 -0400

    Adjust the styling and spacing in the Locations-Banner-Grid

--- responsive-layout/src/modules/location-banner-grid/atoms/StyledButton.ts
+++ responsive-layout/src/modules/location-banner-grid/atoms/StyledButton.ts
@@ -4,10 +4,12 @@ import styled from "@emotion/styled";
 const StyledButton = styled(Button)`
   font-family: Helvetica Neue;
   font-size: 11px;
-  text-align: left;
+  white-space: nowrap;
+  text-align: center;
   color: white;
   &:hover {
-    background-color: lightblue;
+    background-color: #990000;
+    opacity: 1;
   },
 `;
 
--- responsive-layout/src/modules/location-banner-grid/organisms/LocationGrid.tsx
+++ responsive-layout/src/modules/location-banner-grid/organisms/LocationGrid.tsx
@@ -1,38 +1,42 @@
-import React from "react";
 import Grid from "@mui/material/Unstable_Grid2";
-import { Typography, Button } from "@mui/material";
 import { StyledGrid, StyledButton } from "../atoms";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import BusinessIcon from "@mui/icons-material/Business";
 
 const LocationBannerGrid = () => {
   return (
     <StyledGrid
       container
-      spacing={2}
       alignItems="center"
-      direction={"row"}
-      justifyContent="space-evenly"
+      paddingLeft={"2%"}
     >
-      <Grid xs={3} lg={1}>
-        <StyledButton>10454</StyledButton>
+      <Grid xs={2} lg={1}>
+        <StyledButton>
+          <LocationOnIcon fontSize="small" style={{ marginRight: '4px' }}/>
+          10454
+        </StyledButton>
       </Grid>
-      <Grid xs={3} lg={1}>
-        <StyledButton>Harlem 117</StyledButton>
+      <Grid xs={2} lg={1}>
+        <StyledButton style={{ textTransform: 'none' }}>
+          <BusinessIcon fontSize="small" style={{ marginRight: '4px' }}/>
+          Harlem 117th St
+        </StyledButton>
       </Grid>
-      <Grid xs={6} lg={5}></Grid>
+      <Grid xs={8} lg={5}></Grid>
       <Grid lg={1} sx={{ display: { xs: "none", lg: "flex" } }}>
-        <StyledButton>Registry</StyledButton>
-      </Grid>
-      <Grid lg={1} sx={{ display: { xs: "none", lg: "flex" } }}>
-        <StyledButton>Weekly Ad</StyledButton>
-      </Grid>
-      <Grid lg={1} sx={{ display: { xs: "none", lg: "flex" } }}>
-        <StyledButton>RedCard</StyledButton>
-      </Grid>
-      <Grid lg={1} sx={{ display: { xs: "none", lg: "flex" } }}>
-        <StyledButton>Target Circle</StyledButton>
+        <StyledButton style={{ textTransform: 'none' }}>Registry</StyledButton>
       </Grid>
       <Grid lg={1} sx={{ display: { xs: "none", lg: "flex" } }}>
-        <StyledButton>Find Stores</StyledButton>
+        <StyledButton style={{ textTransform: 'none' }}>Weekly Ad</StyledButton>
+      </Grid>
+      <Grid lg={1} sx={{ display: { xs: "none", lg: "flex" } }}>
+        <StyledButton style={{ textTransform: 'none' }}>RedCard</StyledButton>
+      </Grid>
+      <Grid lg={1} sx={{ display: { xs: "none", lg: "flex" } }}>
+        <StyledButton style={{ textTransform: 'none' }}>Target Circle</StyledButton>
+      </Grid>
+      <Grid lg={1} sx={{ display: { xs: "none", lg: "flex" } }}>
+        <StyledButton style={{ textTransform: 'none' }}>Find Stores</StyledButton>
       </Grid>
     </StyledGrid>
   );

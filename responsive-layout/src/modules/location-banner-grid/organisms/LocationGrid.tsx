5dddc1f10e7509dce9f32303ac64913e0c8f6430
alopez-axiom <alopez@axiomcloud.ai>
2023-10-15 19:35:51 -0400

    Update the location banner grid for medium sized screens

--- responsive-layout/src/modules/location-banner-grid/organisms/LocationGrid.tsx
+++ responsive-layout/src/modules/location-banner-grid/organisms/LocationGrid.tsx
@@ -6,34 +6,34 @@ import BusinessIcon from "@mui/icons-material/Business";
 const LocationBannerGrid = () => {
   return (
     <StyledGrid container alignItems="center" paddingLeft={"2%"}>
-      <Grid xs={2} lg={1}>
+      <Grid xs={2} md={1}>
         <StyledButton>
           <LocationOnIcon fontSize="small" style={{ marginRight: "4px" }} />
           10454
         </StyledButton>
       </Grid>
-      <Grid xs={2} lg={1}>
+      <Grid xs={2} md={1}>
         <StyledButton style={{ textTransform: "none" }}>
           <BusinessIcon fontSize="small" style={{ marginRight: "4px" }} />
           Harlem 117th St
         </StyledButton>
       </Grid>
-      <Grid xs={8} lg={5}></Grid>
-      <Grid lg={1} sx={{ display: { xs: "none", lg: "flex" } }}>
+      <Grid xs={8} md={5}></Grid>
+      <Grid md={1} sx={{ display: { xs: "none", md: "flex" } }}>
         <StyledButton style={{ textTransform: "none" }}>Registry</StyledButton>
       </Grid>
-      <Grid lg={1} sx={{ display: { xs: "none", lg: "flex" } }}>
+      <Grid md={1} sx={{ display: { xs: "none", md: "flex" } }}>
         <StyledButton style={{ textTransform: "none" }}>Weekly Ad</StyledButton>
       </Grid>
-      <Grid lg={1} sx={{ display: { xs: "none", lg: "flex" } }}>
+      <Grid md={1} sx={{ display: { xs: "none", md: "flex" } }}>
         <StyledButton style={{ textTransform: "none" }}>RedCard</StyledButton>
       </Grid>
-      <Grid lg={1} sx={{ display: { xs: "none", lg: "flex" } }}>
+      <Grid md={1} sx={{ display: { xs: "none", md: "flex" } }}>
         <StyledButton style={{ textTransform: "none" }}>
           Target Circle
         </StyledButton>
       </Grid>
-      <Grid lg={1} sx={{ display: { xs: "none", lg: "flex" } }}>
+      <Grid md={1} sx={{ display: { xs: "none", md: "flex" } }}>
         <StyledButton style={{ textTransform: "none" }}>
           Find Stores
         </StyledButton>
